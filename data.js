d2e01973c6ba5172a18f4d06a992ffc75a6af6c7
krquinton <nscrexgst.dotr@gmail.com>
2023-05-22 23:17:06 +0800

    update

---
+++ data.js
@@ -0,0 +1,219 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-site-1---a",
+      "name": "Site 1 - A",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.9848419176219183,
+        "pitch": 0.4835828169375933,
+        "fov": 1.4385746071217929
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.4326222740444283,
+          "pitch": 0.00516260965146742,
+          "rotation": 0,
+          "target": "2-site-1---c"
+        },
+        {
+          "yaw": -1.074241795241747,
+          "pitch": -0.0809225880617852,
+          "rotation": 0,
+          "target": "1-site-1---b"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-site-1---b",
+      "name": "Site 1 - B",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.541313328706142,
+        "pitch": 0.6331234523231846,
+        "fov": 1.4385746071217929
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.3186148424784196,
+          "pitch": 0.5563261944795528,
+          "rotation": 0,
+          "target": "1-site-1---b"
+        },
+        {
+          "yaw": 2.479987924754809,
+          "pitch": 0.17041375013885407,
+          "rotation": 0,
+          "target": "2-site-1---c"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-site-1---c",
+      "name": "Site 1 - C",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.2190329661694541,
+        "pitch": 0.5312751819314805,
+        "fov": 1.4385746071217929
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.0649496856143177,
+          "pitch": 0.2573289971507027,
+          "rotation": 0,
+          "target": "0-site-1---a"
+        },
+        {
+          "yaw": -2.9183856520540097,
+          "pitch": 0.019686800980426966,
+          "rotation": 0,
+          "target": "1-site-1---b"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-site-2---a",
+      "name": "Site 2 - A",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.3605658856566762,
+        "pitch": 1.5707963267948966,
+        "fov": 1.4385746071217929
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.5914944206669794,
+          "pitch": 0.47918902136008157,
+          "rotation": 0,
+          "target": "4-site-2---b"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-site-2---b",
+      "name": "Site 2 - B",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.3559779320035403,
+          "pitch": -0.13668700717255078,
+          "rotation": 0,
+          "target": "3-site-2---a"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Capalonga (CAMADA)",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
